664e495e14f75810231a2c2d6eb5dd0bd88aebee
Kevin Huang <khuang771@gmail.com>
2018-01-24 12:55:16 +1100

    ex4

---
+++ ex4.py
@@ -0,0 +1,9 @@
+cars = 100
+space_in_a_car = 4.0
+driveres = 30
+passengers = 90
+cars_not_driven = cars - driver
+cars_driven = driverescarpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+print("There are", cars, "Cars Available")
