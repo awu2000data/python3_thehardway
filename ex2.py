35f2616efa02b785aa44cb6a8965fae2034731b6
Kevin Huang <khuang771@gmail.com>
2018-01-24 12:29:11 +1100

    ex 1,2

---
+++ ex2.py
@@ -0,0 +1,14 @@
+#comments (octothorpe)
+
+# A comment, this is so you can read your program later
+# Anything after the # is ignored by python
+
+print("I could have code like this") # and this is ignored
+
+#print ("This won't run")
+
+print ("This will run.")
+
+
+# print ("Hi # There") will still work because the # is in a STRING
+# hence they are considered as just characters
